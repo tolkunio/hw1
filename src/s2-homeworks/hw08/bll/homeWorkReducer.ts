15916a3b2c174a52ff3bf3d4bf34daa4bab1da83
Omurbekova Tolkun <toprog27@gmail.com>
2023-05-16 16:18:00 +0600

    -HW8 done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,41 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
+    const sortByNameUpCallback = (name1: string, name2: string) => {
+        if (name1 < name2) {
+            return -1;
+        }
+        if (name1 > name2) {
+            return 1;
+        }
+        // names must be equal
+        return 0;
+    }
+    const sortByNameDownCallback = (name1: string, name2: string) => {
+        if (name1 > name2) {
+            return -1;
+        }
+        if (name1 < name2) {
+            return 1;
+        }
+        // names must be equal
+        return 0;
+    }
     switch (action.type) {
         case 'sort': { // by name
+            let sortedUsers:UserType[]=[];
+            if (action.payload == 'up') {
+                sortedUsers = state.sort((a,b)=>sortByNameUpCallback(a.name,b.name));
+            }
+            if (action.payload == 'down') {
+                sortedUsers = state.sort((a, b)=>sortByNameDownCallback(a.name,b.name));
+            }
 
-            return state // need to fix
+            return sortedUsers // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+           return state.filter(u=>u.age>action.payload)
         }
         default:
             return state
