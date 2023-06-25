07e51d1d64a6605147d2e3da9775f91140beca8e
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-06-25 23:39:48 +0600

    -change base url

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -25,7 +25,7 @@ const HW13 = () => {
         const url =
             x === null
                 ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
-                : 'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test'
+                : 'https://samurai.it-incubator.io/api/3.0/homework/test'
 
         setCode('');
         setImage('');
@@ -88,7 +88,6 @@ const HW13 = () => {
                         onClick={send(false)}
                         xType={'secondary'}
                         disabled={isLoading}
-                        // дописать
 
                     >
                         Send false
